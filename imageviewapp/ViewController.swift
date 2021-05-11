8cb5918630ec48c17907828f19ffef52590a6724
gohardsun <ahnmyungsun95@gmail.com>
2021-05-12 01:00:23 +0900

    fist ios imageapp

--- imageviewapp/ViewController.swift
+++ imageviewapp/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var textInput: UITextField!
+    
+    var imageCat: UIImage?
+    var imageCatDog: UIImage?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageCat = UIImage(named: "cat.jpeg")
+        imageCatDog = UIImage(named: "catanddog.jpeg")
     }
 
-
+    @IBAction func imageRefresh(_ sender: UIButton) {
+        if textInput.text == "cat"{
+            imageView.image = imageCat
+        }
+        else if textInput.text == "catanddog"{
+            imageView.image = imageCatDog
+        }
+    }
+    
 }
 
